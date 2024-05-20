21c8511e097fcc218c8d9d533639d9aca7a550d8
Benoit Malenfant <benmalenfant@gmail.com>
2024-05-20 00:00:08 -0400

    format main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <LoRa.h>
 #include <Timer.h>
 
-int rcv_flg=0;
+int rcv_flg = 0;
 int state = 0;
 
 #define REFRESH_PERIOD_MS 350
@@ -20,14 +20,14 @@ int state = 0;
 
 #define VBAT_PIN PA1
 
-
 void onReceive(int packetSize) // ! from isr
 {
     state = LoRa.read();
-    while (LoRa.available()){
+    while (LoRa.available())
+    {
         LoRa.read();
     }
-    rcv_flg=1;
+    rcv_flg = 1;
 }
 
 void LoRa_init()
@@ -37,7 +37,7 @@ void LoRa_init()
     SPI.setSCLK(PA5);
     LoRa.setPins(PA4, PA2, PA3);
     LoRa.setTxPower(20);
-    
+
     LoRa.setSyncWord(0xBE);
     if (!LoRa.begin(904600E3)) // frequency
     {
@@ -59,22 +59,28 @@ void LoRa_send()
 
 void send_at_interval();
 
-double battery_calc_charge(double voltage) {    
-    //calibration table meant for a 12s battery
+double battery_calc_charge(double voltage)
+{
+    // calibration table meant for a 12s battery
     int capacities[] = {100, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35, 30, 25, 20, 15, 10, 5, 0};
     double voltages[] = {4.2, 4.15, 4.11, 4.08, 4.02, 3.98, 3.95, 3.91, 3.87, 3.85, 3.84, 3.82, 3.8, 3.79, 3.77, 3.75, 3.73, 3.71, 3.69, 3.61, 3.27};
 
     // Clip voltage within the range
-    if (voltage > voltages[0]) {
+    if (voltage > voltages[0])
+    {
         voltage = voltages[0];
-    } else if (voltage < voltages[20]) {
+    }
+    else if (voltage < voltages[20])
+    {
         voltage = voltages[20];
     }
 
     // Linear interpolation
     int i;
-    for (i = 0; i < 20; ++i) {
-        if (voltage >= voltages[i]) {
+    for (i = 0; i < 20; ++i)
+    {
+        if (voltage >= voltages[i])
+        {
             break;
         }
     }
@@ -87,18 +93,18 @@ double battery_calc_charge(double voltage) {
 
 void setup()
 {
-    pinMode(ld1,OUTPUT);
-    digitalWrite(ld1,0);
-    pinMode(ld2,OUTPUT);
-    digitalWrite(ld2,0);
-    pinMode(ld3,OUTPUT);
-    digitalWrite(ld3,0);
-    pinMode(ld4,OUTPUT);
-    pinMode(B1,INPUT_PULLUP);
-    pinMode(B2,INPUT_PULLUP);
-    pinMode(B3,INPUT_PULLUP);
+    pinMode(ld1, OUTPUT);
+    digitalWrite(ld1, 0);
+    pinMode(ld2, OUTPUT);
+    digitalWrite(ld2, 0);
+    pinMode(ld3, OUTPUT);
+    digitalWrite(ld3, 0);
+    pinMode(ld4, OUTPUT);
+    pinMode(B1, INPUT_PULLUP);
+    pinMode(B2, INPUT_PULLUP);
+    pinMode(B3, INPUT_PULLUP);
     analogReadResolution(12);
-    digitalWrite(ld4,0);
+    digitalWrite(ld4, 0);
     Serial.begin(9600);
     Serial.println("\nLORA TESTS");
     LoRa_init();
@@ -106,47 +112,52 @@ void setup()
 
 void loop()
 {
-    if(!digitalRead(B3)){
+    if (!digitalRead(B3))
+    {
         float VDDA = 1.2 * 4095 / analogRead(AVREF);
-        float VBAT = 2* VDDA * analogRead(VBAT_PIN)/4095;  
+        float VBAT = 2 * VDDA * analogRead(VBAT_PIN) / 4095;
 
         double charge = battery_calc_charge(VBAT);
         Serial.print("VDDA : ");
         Serial.println(VDDA);
         Serial.print("VBAT : ");
         Serial.println(VBAT);
-        digitalWrite(ld1,charge>75);
-        digitalWrite(ld2,charge>50);
-        digitalWrite(ld3,charge>25);
-        digitalWrite(ld4,1);
+        digitalWrite(ld1, charge > 75);
+        digitalWrite(ld2, charge > 50);
+        digitalWrite(ld3, charge > 25);
+        digitalWrite(ld4, 1);
     }
-    else{
-        digitalWrite(ld1,0);
-        digitalWrite(ld2,0);
-        digitalWrite(ld3,0);
+    else
+    {
+        digitalWrite(ld1, 0);
+        digitalWrite(ld2, 0);
+        digitalWrite(ld3, 0);
     }
-    
-    if(!digitalRead(B2)){
+
+    if (!digitalRead(B2))
+    {
         static unsigned long previousRCV = 0;
         send_at_interval(); // Call the function to check and execute the action
-        if(rcv_flg){
+        if (rcv_flg)
+        {
             rcv_flg = 0;
-            digitalWrite(ld4,state);
+            digitalWrite(ld4, state);
             previousRCV = millis();
         }
-        else if(millis() - previousRCV > TIMEOUT){
+        else if (millis() - previousRCV > TIMEOUT)
+        {
             state = 0;
-            digitalWrite(ld4,state);
+            digitalWrite(ld4, state);
         }
     }
 }
 
-
-
-void send_at_interval() {
+void send_at_interval()
+{
     static unsigned long previousMillis = 0;
     // Check if it's time to execute the action
-    if (millis() - previousMillis >= REFRESH_PERIOD_MS) {
+    if (millis() - previousMillis >= REFRESH_PERIOD_MS)
+    {
         // Save the last time the action was executed
         previousMillis = millis();
         // Call your function here
